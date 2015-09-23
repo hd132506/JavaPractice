200be11edc9495b31482a35aa25cbea8c07388d6
user <user@lg>
2015-09-23 15:10:35 +0900

    실습과제5 재업

--- JavaPractice/src/org/dimigo/collection/MelonChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -21,14 +21,20 @@ public class MelonChart {
 		System.out.println("--<<3위 곡 변경>>--");
 		musics.set(2, new Music("맙소사","황태지"));
 		printList(musics);
+		
+		System.out.println("--<<4위 곡 삭제>>--");
+		musics.remove(3);
+		printList(musics);
+		
+		System.out.println("--<<전체 리스트 삭제>>--");
+		musics.clear();
+		printList(musics);
 	}
 	
 	public static void printList(List<Music> list) {
 		int i=1;
-		for(Music lst: list) {
-			System.out.println(i+"."+lst.toString());
-			i++;
-		}
+		for(Music m: list)
+			System.out.println((i++) + ". " + m.toString());
 		System.out.println();
 	}
 }
